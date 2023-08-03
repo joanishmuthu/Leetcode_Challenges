9ad1905c561f1c3949a6a902442c66eb36a8f52e
Joanish Muthu <52238376+joanishmuthu@users.noreply.github.com>
2023-08-03 11:05:32 +0530

    Time: 133 ms (16.59%), Space: 45 MB (14.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb=new StringBuilder();
-		String[] arr=s.split("");
-		for(int i=0;i<arr.length;i++) {
-			if(Character.isAlphabetic(arr[i].charAt(0)) || Character.isDigit(arr[i].charAt(0))) {
-				if(Character.isAlphabetic(arr[i].charAt(0))) {
-					char t=Character.toLowerCase(arr[i].charAt(0));
-					sb.append(t);
-				}
-				else {
-					sb.append(arr[i]);
-				}
-			}
-		}
-		String rev=sb.toString();
-		sb.reverse();
-		if(sb.toString().equals(rev)) {
-			return true;
-		}
-		else {
-			return false;
-		}
+        String res="";
+    	for(int i=0;i<s.length();i++) {
+        	if(((int)s.charAt(i)>=97 && (int)s.charAt(i)<=122)) {
+        		res=res+s.charAt(i);
+        	}
+        	else if(((int)s.charAt(i)>=65 && (int)s.charAt(i)<=90) ) {
+        		int a=(int)s.charAt(i)+32;
+        		res=res+(char)a;
+        	}
+        	else if((int)s.charAt(i)>=48 && (int)s.charAt(i)<=57) {
+        		res=res+s.charAt(i);
+        	}
+        }
+    	StringBuilder sb=new StringBuilder(res);
+    	sb.reverse();
+    	String r1=sb.toString();
+    	if(res.equals(r1)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 }
